5957af1de49ea6b72722a68218710af25bc56bfb
Polina <polina-postylyakova@yandex.ru>
2023-04-01 16:59:46 +0300

    added Upload schema for MongoDB and routes

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ import mongoose from 'mongoose';
 import multer from 'multer';
 import helmet from 'helmet';
 import cors from 'cors';
-import fs from 'fs';
 
 import { PostController, UserController } from './controllers/index.js';
+import Upload from './models/Upload.js';
 
 import { checkAuth, handleValidationErrors } from './utils/index.js';
 import { commentCreateValidation, fileFilter, postCreateValidation, registerValidation } from './validations.js';
-import * as path from 'path';
 
 mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGODB_URI)
@@ -35,56 +34,116 @@ const app = express();
 //     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 //     next();
 // });
+const storage = multer.memoryStorage();
 
-const storage = multer.diskStorage({
-    destination: (_, __, cb) => {
-        if (!fs.existsSync(path.resolve(process.cwd(), 'uploads'))) {
-            fs.mkdirSync(path.resolve(process.cwd(), 'uploads'));
-        }
-        cb(null, path.resolve(process.cwd(), 'uploads'));
-    },
-    filename: (req, file, cb) => {
-        let extArray = file.mimetype.split("/");
-        let extension = extArray[extArray.length - 1];
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, `${file.fieldname}-${uniqueSuffix}.${extension}`);
-    },
-});
+// const storage = multer.diskStorage({
+//     destination: (_, __, cb) => {
+//         if (!fs.existsSync(path.resolve(process.cwd(), 'uploads'))) {
+//             fs.mkdirSync(path.resolve(process.cwd(), 'uploads'));
+//         }
+//         cb(null, path.resolve(process.cwd(), 'uploads'));
+//     },
+//     filename: (req, file, cb) => {
+//         let extArray = file.mimetype.split("/");
+//         let extension = extArray[extArray.length - 1];
+//         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+//         cb(null, `${file.fieldname}-${uniqueSuffix}.${extension}`);
+//     },
+// });
 
 const upload = multer({ storage, fileFilter: fileFilter });
 
 app.use(express.json());
 app.use(cors());
 
-app.use('/uploads', express.static('uploads'));
+// app.use('/uploads', express.static('uploads'));
 
 app.post('/auth/login', handleValidationErrors, UserController.login);
 app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
 
-app.get('/uploads', (req, res) => {
-    let images = [];
-    fs.readdir(path.resolve(process.cwd(), 'uploads'), (err, files) => {
-        if (!err) {
-            files.forEach(file => {
-                images.push(file);
-            })
-            res.json({ images: images});
-        } else {
-            console.log(err);
-        }
-    });
-});
-app.post('/uploads', checkAuth, upload.single('image'), (req, res) => {
-    if (req.fileValidationError) {
-        return res.status(400).json({
-            message: req.fileValidationError,
+// app.get('/uploads', (req, res) => {
+//     let images = [];
+//     fs.readdir(path.resolve(process.cwd(), 'uploads'), (err, files) => {
+//         if (!err) {
+//             files.forEach(file => {
+//                 images.push(file);
+//             })
+//             res.json({ images: images });
+//         } else {
+//             console.log(err);
+//         }
+//     });
+// });
+
+app.get('/uploads/:imageName', async (req, res) => {
+    try {
+        const imageName = req.params.imageName;
+
+        Upload.findOne({
+                fileName: imageName,
+            },
+            (err, doc) => {
+                if (!doc) {
+                    return res.status(404).json({
+                        message: 'Изображение не найдено',
+                    });
+                }
+
+                if (err) {
+                    console.error(err);
+                    return res.status(500).json({
+                        message: 'Не удалось вернуть изображение',
+                    });
+                }
+
+                res.contentType(doc.contentType);
+                res.json(doc);
+            },
+        );
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            message: 'Не удалось получить изображение',
         });
     }
+});
+app.post('/uploads', checkAuth, upload.single('image'), async (req, res) => {
+    try {
+        if (req.fileValidationError) {
+            return res.status(400).json({
+                message: req.fileValidationError,
+            });
+        }
 
-    res.json({
-        url: `/uploads/${req.file?.filename}`,
-    });
+        console.log(req.file);
+        console.log(req.file.filename);
+
+        let extArray = req.file.mimetype.split("/");
+        let extension = extArray[extArray.length - 1];
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        let imageUploadObject = {
+            file: {
+                data: req.file.buffer,
+                contentType: req.file.mimetype,
+            },
+            fileName: `${req.file.fieldname}-${uniqueSuffix}.${extension}`,
+            // fileName: req.file.filename,
+        };
+        const uploadObject = new Upload(imageUploadObject);
+        const uploadProcess = await uploadObject.save();
+
+        res.json(uploadProcess);
+        // res.json({
+        //     url: `/uploads/${req.file?.filename}`,
+        // });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            message: 'Не удалось загрузить изображение',
+        });
+    }
 });
 
 app.get('/tags', PostController.getLastTags);

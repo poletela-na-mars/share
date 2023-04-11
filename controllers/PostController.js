d8ad0b71e3971329c1bf6644e5b9c2d6e292808d
Polina <polina-postylyakova@yandex.ru>
2023-04-11 22:20:09 +0300

    added await for findOneAndDelete in remove image function

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -9,7 +9,7 @@ const removeImage = async (oldImageUrl, res) => {
         const imageUrl = oldImageUrl;
 
         if (imageUrl) {
-            await Upload.findOneAndDelete({
+            Upload.findOneAndDelete({
                     fileName: imageUrl.replace('/uploads/', ''),
                 },
                 (err, doc) => {
